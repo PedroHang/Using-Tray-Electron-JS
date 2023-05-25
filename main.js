98837bd4a0e5825d7ae3450d79950352f4d858b9
Pedro Lucas Azevedo Hang <hangpedroo@hotmail.com>
2023-05-25 19:37:27 -0300

    Test for profiling

--- main.js
+++ main.js
@@ -1,59 +1,42 @@
 const { app, BrowserWindow, Tray, Menu } = require('electron');
-const inspector = require('inspector');
-const fs = require('fs');
-const session = new inspector.Session();
 
-session.connect();
 
-let myWindow = null; 
+let myWindow = null;
 
 
 
-session.post('Profiler.enable', () => {
-    session.post('Profiler.start', () => {
-      
-        app.whenReady().then(() => {
-            myWindow = new BrowserWindow({
-                width: 800,
-                height: 600,
-                webPreferences: {
-                    nodeIntegration: true
-                }
-            });
-        
-            myWindow.loadFile('index.html');
-        
-            const tray = new Tray('assets/iconn.png');
-        
-            const contextMenu = Menu.buildFromTemplate([
-                { label: 'My Drive', click: createSmallWindow },
-                { label: 'Preferences', click: createSmallWindow },
-                { label: 'Quit', role: 'quit' } 
-            ]);
-            tray.setToolTip('MY DRIVE');
-            tray.setContextMenu(contextMenu);
-        
-            tray.on('click', createSmallWindow); 
-        });
-        
-        function createSmallWindow() {
-            const smallWindow = new BrowserWindow({
-                width: 400,
-                height: 300,
-                webPreferences: {
-                    nodeIntegration: true
-                }
-            });
-        
-            smallWindow.loadFile('small.html');
+app.whenReady().then(() => {
+    const tray = new Tray('assets/iconn.png');
+
+    const contextMenu = Menu.buildFromTemplate([
+        { label: 'My Drive', click: createSmallWindow },
+        { label: 'Preferences', click: createSmallWindow },
+        { label: 'Quit', role: 'quit' }
+    ]);
+    tray.setToolTip('MY DRIVE');
+    tray.setContextMenu(contextMenu);
+
+    tray.on('click', createSmallWindow);          
+
+
+      // Profiler: 33,2mb somente o ícone na tray (memória)
+
+
+
+});
+
+function createSmallWindow() {
+    const smallWindow = new BrowserWindow({
+        width: 400,
+        height: 300,
+        webPreferences: {
+            nodeIntegration: true
         }
-  
-      // some time later...
-      session.post('Profiler.stop', (err, { profile }) => {
-        // Write profile to disk, upload, etc.
-        if (!err) {
-          fs.writeFileSync('./profile.cpuprofile', JSON.stringify(profile));
-        }
-      });
     });
-  });
+
+    smallWindow.loadFile('small.html');
+}
+
+
+
+
